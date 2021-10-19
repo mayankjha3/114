ed3da588f51970fc0f3834d7f28eb3b7ff3a0724
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2021-10-19 19:08:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX = 0;
+noseY = 0;
 function preload() {
-
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
@@ -15,6 +17,7 @@ function setup() {
 
 function draw() {
     image(video , 0 , 0 , 300 , 300);
+    image(mustache , noseX , noseY , 30 ,30)
 }
 
 function take_snapshot(){
@@ -28,7 +31,9 @@ function modalLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
