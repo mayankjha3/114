907fb9d00b1e71495b6651b538d48dbc4a1741af
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2021-10-25 15:47:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
         noseX = results[0].pose.nose.x - 15;
-        noseY = results[0].pose.nose.y;
+        noseY = results[0].pose.nose.y + 3;
         console.log("nose x = " + noseX);
         console.log("nose y = " + noseY);
     }
